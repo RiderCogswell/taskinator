503584d356133701fdd95d259c7429bf289e919d
Ridercogswell <ridercogswell@gmail.com>
2022-01-19 22:15:40 -0800

    form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,17 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
-    
+
     var taskNameInput = document.querySelector("input[name = 'task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // checkif the input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    } 
+
+    formEl.reset();
 
     // package up data as an object
     var taskDataObject = {
